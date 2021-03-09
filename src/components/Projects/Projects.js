9a2fcb2699b0b1b1be2b3c26a7b6f56c20cfbf74
Kévin TSI <kevtsiii@gmail.com>
2021-03-09 21:59:42 +0100

    Page projets (WIP)

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,15 @@
+import { React } from "react"
+import "./Projects.css"
+
+const Projects = () => {
+    return (
+        <div className="projects-container fade">
+            <div><h2>Mes projets professionnels</h2></div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Projects
